fb2204c7ca265a55da866b32493e8b4757738879
Piero Lujan Pedreschi <pierolujanpedreschi@macbookpro.lan>
2024-11-10 16:46:56 -0800

    create localStorage and display object values when page loads: '

---
+++ main.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('input');
+const div = document.querySelector('div');
+const submit = document.querySelector('.submit');
+
+submit.addEventListener("click", appendNewObject);
+
+const myArray = JSON.parse(localStorage.getItem('myArray')) || [];
+loadPage();
+
+function loadPage() {
+    console.log(myArray);
+    localStorage.setItem('myArray', JSON.stringify(myArray));
+    for (let i = 0; i < JSON.parse(localStorage.getItem('myArray')).length; i++) {
+        const h2 = document.createElement('h2');
+        div.appendChild(h2);
+        h2.innerHTML = JSON.parse(localStorage.getItem('myArray'))[i].phrase;
+    }
+}
+
+localStorage.clear();
+
+function appendNewObject() {
+    const newObj = {phrase: input.value, name: input.value}
+    myArray.push(newObj);
+    let length = myArray.length;
+    localStorage.setItem('myArray', JSON.stringify(myArray));
+    const h2 = document.createElement('h2');
+    div.appendChild(h2);
+    h2.innerHTML = JSON.parse(localStorage.getItem('myArray'))[length - 1].phrase;
+}
